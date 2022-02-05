ce1599d02639a1acae634866b39b16e60f4bc1af
Ihor Baierzdorf <baierzdorf.igor@gmail.com>
2022-02-05 13:32:50 +0200

    task_1

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const ingredientRef = ingredients.forEach(() => {
+  const itemRef = document.createElement("li");
+  (ingredient) => ingredient + "";
+  itemRef.textContent = ingredients;
+  itemRef.classList.add("item");
+  console.log(itemRef);
+});
